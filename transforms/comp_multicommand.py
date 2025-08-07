dea099f241df35025e484568627da977d21252ac
Trico Everfire <bobbys@live.nl>
2025-08-07 04:06:08 +0200

    Add/Change: additions/changes based on feedback. Also icon added.

--- transforms/comp_multicommand.py
+++ transforms/comp_multicommand.py
@@ -3,84 +3,60 @@ from srctools import conv_bool, conv_float, Entity, Output
 from srctools.logger import get_logger
 
 from hammeraddons.bsp_transform import trans, Context
+from hammeraddons.bsp_transform.common import strip_cust_keys
 
-@trans('comp_multi_client_command')
-def comp_multi_client_command(ctx: Context):
+@trans('comp_multi_command')
+def comp_multi_command(ctx: Context):
 
-    multicommands = ctx.vmf.by_class['comp_multi_client_command']
+    multicommands = ctx.vmf.by_class['comp_multi_command']
+
     if(len(multicommands) < 1):
         return
 
-    clientcommand = get_command_executor(ctx, "client")
-
     for comp_ent in multicommands:
-        relay = process_multicommand_entties(ctx, comp_ent, clientcommand)
-        relay["targetname"] = comp_ent["targetname"]
-    pass
 
-@trans('comp_multi_server_command')
-def comp_multi_server_command(ctx: Context):
+        command_caller = get_command_executor(ctx, comp_ent["type"].casefold())
+        command_list:list[str] = []
 
-    multicommands = ctx.vmf.by_class['comp_multi_server_command']
-    if(len(multicommands) < 1):
-        return
+        for i in range(16):
+            command = comp_ent['command_' + (str(i))]
+            if(len(command) < 1):
+                continue
+            command_list.append(command)
+        joined_commands = "; ".join(command_list)
 
-    servercommand = get_command_executor(ctx, "server")
+        match comp_ent['mode'].casefold():
+            case 'spawn':
+                comp_ent['classname'] = 'logic_auto'
+                comp_ent['targetname'] = ''
+                output = 'OnMapSpawn'
+            case 'trigger':
+                comp_ent['classname'] = 'logic_relay'
+                output = 'OnTrigger'
+            case _:
+                raise ValueError(f'Invalid comp_multi_command mode "{comp_ent['mode']}"!')
 
-    for comp_ent in multicommands:
-        relay = process_multicommand_entties(ctx, comp_ent, servercommand)
-        relay["targetname"] = comp_ent["targetname"]
-    pass
+        comp_ent.add_out(Output(output, command_caller, "Command", joined_commands, 0))
+        strip_cust_keys(comp_ent)
+ 
 
-@trans('comp_auto_multi_client_command')
-def comp_auto_multi_client_command(ctx: Context):
-
-    multicommands = ctx.vmf.by_class['comp_auto_multi_client_command']
-    if(len(multicommands) < 1):
-        return
-
-    clientcommand = get_command_executor(ctx, "client")
-
-    for comp_ent in multicommands:
-        relay = process_multicommand_entties(ctx, comp_ent, clientcommand)
-        relay["spawnflags"] = 1 # We want this entity to delete itself.
-        relay.add_out(Output("OnSpawn","!self","Trigger","",0))
-    pass
-
-@trans('comp_auto_multi_server_command')
-def comp_auto_multi_server_command(ctx: Context):
-
-    multicommands = ctx.vmf.by_class['comp_auto_multi_server_command']
-    if(len(multicommands) < 1):
-        return
-
-    servercommand = get_command_executor(ctx, "server")
-
-    for comp_ent in multicommands:
-        relay = process_multicommand_entties(ctx, comp_ent, servercommand)
-        relay["spawnflags"] = 1 # We want this entity to delete itself.
-        relay.add_out(Output("OnSpawn","!self","Trigger","",0))
-    pass
-
-
-def process_multicommand_entties(ctx:Context, comp_ent: Entity, command_caller: Entity) -> 'Entity':
-
-    comp_ent.remove()
-    command_list:list[str] = []
-    for i in range(16):
-        command_list.append(comp_ent['command_' + (str(i))])
-    joined_commands = "; ".join(command_list)
-    relay = ctx.vmf.create_ent("logic_relay")
-    relay.add_out(Output("OnTrigger", command_caller, "Command", joined_commands, 0))
-
-    return relay
 
 def get_command_executor(ctx:Context, type:str) -> Entity:
 
-    command = ctx.vmf.by_target['cmp_multi_'+type+'command']
+    match type:
+        case 'client':
+            entity_name = 'point_clientcommand'
+        case 'server':
+            entity_name = 'point_servercommand'
+        case 'multiplayer':
+            entity_name = 'point_broadcastclientcommand'
+        case _:
+            raise ValueError(f'Invalid command entity type "{type}"!')
+
+    command = ctx.vmf.by_target[entity_name]
 
     if(len(command) < 1): # If it doesn't exist, create one.
-        command = ctx.vmf.create_ent('point_'+type+'command')
-        command["targetname"] = 'cmp_multi_'+type+'command'
+        command = ctx.vmf.create_ent(entity_name)
+        command["targetname"] = f'cmp_multi_{entity_name}'
 
     return command
