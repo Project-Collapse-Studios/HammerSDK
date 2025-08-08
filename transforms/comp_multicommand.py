373327dc520d1912e037cb46378bf820df899bfa
Spencer Brown <spencerb21@live.com>
2025-08-08 13:18:30 +1000

    Preserve command order, account for max output limit

--- transforms/comp_multicommand.py
+++ transforms/comp_multicommand.py
@@ -1,5 +1,5 @@
 """Specifies a list of commands, which will be bundled into inputs and fired on map spawn or on trigger."""
-from srctools import Entity, Output, VMF
+from srctools import Entity, Output, VMF, conv_int
 
 from hammeraddons.bsp_transform import trans, Context
 from hammeraddons.bsp_transform.common import strip_cust_keys, ent_description
@@ -12,11 +12,20 @@ def comp_multi_command(ctx: Context) -> None:
     for comp_ent in ctx.vmf.by_class['comp_multi_command']:
         command_caller = get_command_executor(ctx.vmf, command_ents, comp_ent)
 
-        command_list: list[str] = [
-            command for name, command in comp_ent.items()
-            if name.startswith('command_')
-        ]
-        joined_commands = "; ".join(command_list)
+        command_list: list[tuple[int, int, str]] = []
+        for name, command in comp_ent.items():
+            # Hammer allows some_kv#2, for duplicates. And we support command_4. So check
+            # for both.
+            if not name.startswith('command_'):
+                continue
+            name = name.removeprefix('command_')
+            if '#' in name:
+                a, b = name.split('#', 1)
+                command_list.append((conv_int(a), conv_int(b), command))
+            else:
+                command_list.append((conv_int(name), 0, command))
+
+        command_list.sort()
 
         match comp_ent['mode'].casefold():
             case 'spawn':
@@ -32,7 +41,21 @@ def comp_multi_command(ctx: Context) -> None:
                     f'for {ent_description(comp_ent)}'
                 )
 
-        comp_ent.add_out(Output(output, command_caller, "Command", joined_commands))
+        buffer = []
+        buf_size = 0
+        for _, _, command in command_list:
+            size = len(command) + 1
+            if buf_size + size >= 250:
+                # Output limit hit, produce one IO.
+                comp_ent.add_out(Output(
+                    output, command_caller, "Command",
+                    ";".join(buffer),
+                ))
+                buffer.clear()
+            buffer.append(command)
+            buf_size += size
+        if buffer:
+            comp_ent.add_out(Output(output, command_caller, "Command", ";".join(buffer)))
         strip_cust_keys(comp_ent)
 
 
